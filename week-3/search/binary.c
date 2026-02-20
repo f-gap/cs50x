be3d9a7b10ad768f14e354e07406e68b77e00469
Gabriel <gabrielportela224.gmail.com>
2026-02-20 17:02:47 -0300

    tentando denovo

--- week-3/search/binary.c
+++ week-3/search/binary.c
@@ -6,9 +6,10 @@ int main()
 {
     int amount;
     int list_amounts[] = {10,20,30,40,50,60,70,80,90,100};
+    int lenght = sizeof(list_amounts)/sizeof(list_amounts[0]);
     printf("Digit an amount from 10 to 100: ");
     scanf("%d", &amount);
-    if (binary_search(amount,sizeof(list_amounts), list_amounts) == true)
+    if (binary_search(amount,lenght, list_amounts) == true)
     {
         printf("This amount exist in list of amounts!\n");
     }
@@ -20,17 +21,25 @@ int main()
 
 bool binary_search(int value, int lenght, int *list_values)
 {
+    int n = lenght/2;
+    int max = lenght;
     do
     {
-        if (value == list_values[lenght/2])
+        if (n < 0 || n > max)
+        {
+            return false;
+        }
+        if (value == list_values[n])
         {
             return true;
         }
-        else if(value > list_values[lenght/2])
+        else if(value < list_values[n])
         {
-            
+            n-=n/2;
         }
-        
+        else if(value > list_values[n])
+        {
+            n+=n/2;
+        }
     } while (true);
-    
 }
