cda2af1085a6c6e28110475b24e1f07a8278c163
Gabriel <gabrielportela224.gmail.com>
2026-03-20 20:42:50 -0300

    feat: função de processar os votos

--- week-3/runoff/runoff.c
+++ week-3/runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -68,8 +69,17 @@ int main(int argc, string argv[])
         // Query for each rank
         for (int j = 0; j < candidate_count; j++)
         {
+            string temp;
             string name = get_string("Rank %i: ", j + 1);
-
+            while (j>0)
+            {
+                if (strcmp(name,temp) == 0)
+                {
+                    printf("Invalid vote.\n");
+                    return 6;    
+                }   
+            }
+            strcpy(temp,name);
             // Record vote, unless it's invalid
             if (!vote(i, j, name))
             {
@@ -126,7 +136,14 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
