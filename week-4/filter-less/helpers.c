998cb8d6a55808a8f4cb5b01dd0d888a72d19fe1
gap <gabrielportela224@gmail.com>
2026-04-06 16:26:03 -0300

    feature: add função blur

--- week-4/filter-less/helpers.c
+++ week-4/filter-less/helpers.c
@@ -42,11 +42,59 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0,n = width - j - 1; j < n; j++,n--)
+        {
+            RGBTRIPLE temp = image[i][n];
+            image[i][n] = image[i][j];
+            image[i][j] = temp;
+        }     
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Create a copy of image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
     return;
+}
+
+RGBTRIPLE average(int height, int width, RGBTRIPLE copy[height][width])
+{
+    BYTE denominator = 0;
+    RGBTRIPLE average;
+    for (int i = height - 1; i <= height + 1; i++)
+    {
+        for (int j = width - 1; j <= width + 1; j++)
+        {
+            average.rgbtBlue = 0;
+            average.rgbtGreen = 0;
+            average.rgbtRed = 0;
+        }
+    }
+    for (int i = height - 1; i <= height + 1; i++)
+    {
+        for (int j = width - 1; j <= width + 1; j++)
+        {
+            average.rgbtBlue += copy[i][j].rgbtBlue;
+            average.rgbtGreen += copy[i][j].rgbtGreen;
+            average.rgbtRed += copy[i][j].rgbtRed;
+            denominator++;
+        }
+    }
+    average.rgbtBlue /= denominator;
+    average.rgbtGreen /= denominator;
+    average.rgbtRed /= denominator;
+    return average;
 }
